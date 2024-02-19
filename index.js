d6d9f6a1f5a590c3853de879c082f0a640719277
Moin Khan <moinkhan1119rja@gmail.com>
2024-02-19 11:59:23 +0000

    input text  problem solved

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ pNumber.addEventListener('input', function () {
     }
     else{
         pNumber.value = '';
-    return alert('First select your seat and enter your [hone number again to continue.')
+    return alert('First select your seat and enter your phone number again to continue.')
     }
 })
 nextBtn.addEventListener('click', function () {
